15804848cd94aee9187e547460da36714ecc5c34
Nathanial Walton <nathanial28@gmail.com>
2022-09-17 10:11:43 -0600

    added icons

---
+++ card/app.js
@@ -0,0 +1,103 @@
+/*
+
+const  = {
+  title: "",
+  synopsis: "The synopsis",
+  subtitle: "",
+  author: "",
+  image: ""
+};
+
+document.getElementById("").innerHTML =
+  .author;
+
+document.getElementById("").innerHTML =
+  .title;
+
+  document.getElementById("").innerHTML =
+  .subtitle;
+
+document.getElementById("").innerHTML =
+  .synopsis;
+
+document.getElementById("").innerHTML =
+  "<img src='" +
+  .image +
+  "' class='img' alt=''>";
+
+*/
+
+//   ////////////////
+const the_fall_of_carthage = {
+  title: "The Fall of Carthage",
+  synopsis:
+    "The struggle between Rome and Carthage in the Punic Wars was arguably the greatest and most desperate conflict of antiquity. The forces involved and the casualties suffered by both sides were far greater than in any wars fought before the modern era, while the eventual outcome had far-reaching consequences for the history of the Western World, namely the ascendancy of Rome. An epic of war and battle, this is also the story of famous generals and leaders: Hannibal, Fabius Maximus, Scipio Africanus, and his grandson Scipio Aemilianus, who would finally bring down the walls of Carthage.",
+  subtitle: "",
+  author: "Adrian Goldsworthy",
+  image:
+    "/images/books/non-fiction/history/rome/republic/the_fall_of_carthage-400.jpg",
+};
+
+document.getElementById("the_fall_of_carthage_author").innerHTML =
+  the_fall_of_carthage.author;
+
+document.getElementById("the_fall_of_carthage_title").innerHTML =
+  the_fall_of_carthage.title;
+
+document.getElementById("the_fall_of_carthage_subtitle").innerHTML =
+  the_fall_of_carthage.subtitle;
+
+document.getElementById("the_fall_of_carthage_synopsis").innerHTML =
+  the_fall_of_carthage.synopsis;
+
+document.getElementById("the_fall_of_carthage_image").innerHTML =
+  "<img src='" + the_fall_of_carthage.image + "' class='img' alt=''>";
+
+///////////////////////////////////
+
+const the_storm_before_the_storm = {
+  title: "the storm before the storm",
+  synopsis:
+    "The Roman Republic was one of the most remarkable achievements in the history of civilization. Beginning as a small city-state in central Italy, Rome gradually expanded into a wider world filled with petty tyrants, barbarian chieftains, and despotic kings. Through the centuries, Rome's model of cooperative and participatory government remained remarkably durable and unmatched in the history of the ancient world. <br /><br />In 146 BC, Rome finally emerged as the strongest power in the Mediterranean. But the very success of the Republic proved to be its undoing. The republican system was unable to cope with the vast empire Rome now ruled: rising economic inequality disrupted traditional ways of life, endemic social and ethnic prejudice led to clashes over citizenship and voting rights, and rampant corruption and ruthless ambition sparked violent political clashes that cracked the once indestructible foundations of the Republic. <br /><br />Chronicling the years 146-78 BC, The Storm Before the Storm dives headlong into the first generation to face this treacherous new political environment. Abandoning the ancient principles of their forbearers, men like Marius, Sulla, and the Gracchi brothers set dangerous new precedents that would start the Republic on the road to destruction and provide a stark warning about what can happen to a civilization that has lost its way.",
+  subtitle: "the beginning of the end of the Roman Republic",
+  author: "mike duncan",
+  image:
+    "/images/books/non-fiction/history/rome/republic/the_storm_before_the_storm-400.jpg",
+};
+
+document.getElementById("the_storm_before_the_storm_author").innerHTML =
+  the_storm_before_the_storm.author;
+
+document.getElementById("the_storm_before_the_storm_title").innerHTML =
+  the_storm_before_the_storm.title;
+
+document.getElementById("the_storm_before_the_storm_subtitle").innerHTML =
+  the_storm_before_the_storm.subtitle;
+
+document.getElementById("the_storm_before_the_storm_synopsis").innerHTML =
+  the_storm_before_the_storm.synopsis;
+
+document.getElementById("the_storm_before_the_storm_image").innerHTML =
+  "<img src='" + the_storm_before_the_storm.image + "' class='img' alt=''>";
+
+/////////////////////////
+
+const rubicon = {
+  title: "rubicon",
+  synopsis:
+    "In 49 B.C., the seven hundred fifth year since the founding of Rome, Julius Caesar crossed a small border river called the Rubicon and plunged Rome into cataclysmic civil war. Tom Holland’s enthralling account tells the story of Caesar’s generation, witness to the twilight of the Republic and its bloody transformation into an empire. From Cicero, Spartacus, and Brutus, to Cleopatra, Virgil, and Augustus, here are some of the most legendary figures in history brought thrillingly to life. Combining verve and freshness with scrupulous scholarship, Rubicon is not only an engrossing history of this pivotal era but a uniquely resonant portrait of a great civilization in all its extremes of self-sacrifice and rivalry, decadence and catastrophe, intrigue, war, and world-shaking ambition.",
+  subtitle: "The Last Years of the Roman Empire",
+  author: "Tom Holland",
+  image: "/images/books/non-fiction/history/rome/republic/rubicon.jpg",
+};
+
+document.getElementById("rubicon_author").innerHTML = rubicon.author;
+
+document.getElementById("rubicon_title").innerHTML = rubicon.title;
+
+document.getElementById("rubicon_subtitle").innerHTML = rubicon.subtitle;
+
+document.getElementById("rubicon_synopsis").innerHTML = rubicon.synopsis;
+
+document.getElementById("rubicon_image").innerHTML =
+  "<img src='" + rubicon.image + "' class='img' alt=''>";
