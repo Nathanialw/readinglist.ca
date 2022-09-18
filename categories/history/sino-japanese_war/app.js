7701f38089b47e4950a115c54be02ef05ab8247c
Nathanial Walton <nathanial28@gmail.com>
2022-09-18 09:23:56 -0600

    added sino-china war page created tempates

---
+++ categories/history/sino-japanese_war/app.js
@@ -0,0 +1,123 @@
+//   ////////////////
+const forgotton_ally = {
+  title: "the forgotton ally",
+  subtitle: "China's World War II, 1937-1945",
+  author: "rana mitter",
+  synopsis:
+    "n 1937, two years before Hitler invaded Poland, Chinese troops clashed with Japanese occupiers in the first battle of World War II. Joining with the United States, the Soviet Union, and Great Britain, China became the fourth great ally in a devastating struggle for its very survival.<br/><br/>Prizewinning historian Rana Mitter unfurls China’s drama of invasi on, resistance, slaughter, and political intrigue as never before. Based on groundbreaking research, this gripping narrative focuses on a handful of unforgettable characters, including Chiang Kai-shek, Mao Zedong, and Chiang’s American chief of staff, “Vinegar Joe” Stilwell. Mitter also recounts the sacrifice and resilience of everyday Chinese people through the horrors of bombings, famines, and the infamous Rape of Nanking.<br/><br/>More than any other twentieth-century event, World War II was crucial in shaping China’s worldview, making Forgotten Ally both a definitive work of history and an indispensable guide to today’s China and its relationship with the West.",
+  image: "./images/forgotton-ally.jpg",
+};
+
+document.getElementById("forgotton_ally_title").innerHTML =
+  forgotton_ally.title;
+
+document.getElementById("forgotton_ally_subtitle").innerHTML =
+  forgotton_ally.subtitle;
+
+document.getElementById("forgotton_ally_author").innerHTML =
+  forgotton_ally.author;
+
+document.getElementById("forgotton_ally_synopsis").innerHTML =
+  forgotton_ally.synopsis;
+
+document.getElementById("forgotton_ally_image").innerHTML =
+  "<img src='" + forgotton_ally.image + "' class='img' alt=''>";
+
+///////////////////////////////////
+//   ////////////////
+const china_at_war = {
+  title: "China at War",
+  subtitle: "Triumph and Tragedy in the Emergence of the New China",
+  author: "Hans van de Ven ",
+  synopsis:
+    "China’s mid-twentieth-century wars pose extraordinary interpretive challenges. The issue is not just that the Chinese fought for such a long time―from the Marco Polo Bridge Incident of July 1937 until the close of the Korean War in 1953―across such vast territory. As Hans van de Ven explains, the greatest puzzles lie in understanding China’s simultaneous external and internal wars. Much is at stake, politically, in how this story is told.<br/><br/>Today in its official history and public commemorations, the People’s Republic asserts Chinese unity against Japan during World War II. But this overwrites the era’s stark divisions between Communists and Nationalists, increasingly erasing the civil war from memory. Van de Ven argues that the war with Japan, the civil war, and its aftermath were in fact of a piece―a singular process of conflict and political change. Reintegrating the Communist uprising with the Sino-Japanese War, he shows how the Communists took advantage of wartime to increase their appeal, how fissures between the Nationalists and Communists affected anti-Japanese resistance, and how the fractious coalition fostered conditions for revolution.<br/><br/>In the process, the Chinese invented an influential paradigm of war, wherein the Clausewitzian model of total war between well-defined interstate enemies gave way to murky campaigns of national liberation involving diverse domestic and outside belligerents. This history disappears when the realities of China’s mid-century conflicts are stripped from public view. China at War recovers them.",
+  image: "./images/china-at-war.jpg",
+};
+
+document.getElementById("china_at_war_title").innerHTML = china_at_war.title;
+
+document.getElementById("china_at_war_subtitle").innerHTML =
+  china_at_war.subtitle;
+
+document.getElementById("china_at_war_author").innerHTML = china_at_war.author;
+
+document.getElementById("china_at_war_synopsis").innerHTML =
+  china_at_war.synopsis;
+
+document.getElementById("china_at_war_image").innerHTML =
+  "<img src='" + china_at_war.image + "' class='img' alt=''>";
+
+///////////////////////////////////
+//   ////////////////
+const the_generalissimo = {
+  title: "The Generalissimo",
+  subtitle: "Chiang Kai-shek and the Struggle for Modern China",
+  author: "Jay Taylor",
+  synopsis:
+    "One of the most momentous stories of the last century is China’s rise from a self-satisfied, anti-modern, decaying society into a global power that promises to one day rival the United States. Chiang Kai-shek, an autocratic, larger-than-life figure, dominates this story. A modernist as well as a neo-Confucianist, Chiang was a man of war who led the most ancient and populous country in the world through a quarter century of bloody revolutions, civil conflict, and wars of resistance against Japanese aggression.<br/><br/>In 1949, when he was defeated by Mao Zedong―his archrival for leadership of China―he fled to Taiwan, where he ruled for another twenty-five years. Playing a key role in the cold war with China, Chiang suppressed opposition with his “white terror,” controlled inflation and corruption, carried out land reform, and raised personal income, health, and educational levels on the island. Consciously or not, he set the stage for Taiwan’s evolution of a Chinese model of democratic modernization.<br/><br/>Drawing heavily on Chinese sources including Chiang’s diaries, The Generalissimo provides the most lively, sweeping, and objective biography yet of a man whose length of uninterrupted, active engagement at the highest levels in the march of history is excelled by few, if any, in modern history. Jay Taylor shows a man who was exceedingly ruthless and temperamental but who was also courageous and conscientious in matters of state. Revealing fascinating aspects of Chiang’s life, Taylor provides penetrating insight into the dynamics of the past that lie behind the struggle for modernity of mainland China and its relationship with Taiwan.",
+  image: "./images/the-generalissimo.jpg",
+};
+
+document.getElementById("the_generalissimo_title").innerHTML =
+  the_generalissimo.title;
+
+document.getElementById("the_generalissimo_subtitle").innerHTML =
+  the_generalissimo.subtitle;
+
+document.getElementById("the_generalissimo_author").innerHTML =
+  the_generalissimo.author;
+
+document.getElementById("the_generalissimo_synopsis").innerHTML =
+  the_generalissimo.synopsis;
+
+document.getElementById("the_generalissimo_image").innerHTML =
+  "<img src='" + the_generalissimo.image + "' class='img' alt=''>";
+
+///////////////////////////////////
+//   ////////////////
+const shanghai_1937 = {
+  title: "Shanghai 1937",
+  subtitle: "Stalingrad on the Yangtze",
+  author: " Peter Harmsen ",
+  synopsis:
+    "At its height, the Battle of Shanghai involved nearly a million Chinese and Japanese soldiers while sucking in three million civilians as unwilling spectators—and often victims. It turned what had been a Japanese imperialist adventure in China into a general war between the two oldest and proudest civilizations of the Far East. Ultimately, it led to Pearl Harbor and to seven decades of tumultuous history in Asia. The Battle of Shanghai was a pivotal event that helped define and shape the modern world.<br/><br/>In its sheer scale, the struggle for China’s largest city was a sinister forewarning of what was in store only a few years later in theaters around the world. It demonstrated how technology had given rise to new forms of warfare and had made old forms even more lethal. Amphibious landings, tank assaults, aerial dogfights, and—most important—urban combat all happened in Shanghai in 1937. It was a dress rehearsal for World War II—or, perhaps more correctly, it was the inaugural act in the war, the first major battle in the global conflict.<br/><br/>Actors from a variety of nations were present in Shanghai during the three fateful autumn months when the battle raged. The rich cast included China’s ascetic Generalissimo Chiang Kai-shek and his Japanese adversary, General Matsui Iwane, who wanted Asia to rise from disunity, but ultimately pushed the continent toward its deadliest conflict ever. Claire Chennault, later of “Flying Tiger” fame, was among the figures emerging in the course of the campaign, as was First Lady Eleanor Roosevelt. In an ironic twist, Alexander von Falkenhausen, a stern German veteran of the Great War, abandoned his role as a mere advisor to the Chinese army and led it into battle against the Japanese invaders.<br/><br/>Shanghai 1937 fills a gaping chasm in our understanding of the War of Resistance and the Second World War.",
+  image: "./images/shangai-1937.jpg",
+};
+
+document.getElementById("shanghai_1937_title").innerHTML = shanghai_1937.title;
+
+document.getElementById("shanghai_1937_subtitle").innerHTML =
+  shanghai_1937.subtitle;
+
+document.getElementById("shanghai_1937_author").innerHTML =
+  shanghai_1937.author;
+
+document.getElementById("shanghai_1937_synopsis").innerHTML =
+  shanghai_1937.synopsis;
+
+document.getElementById("shanghai_1937_image").innerHTML =
+  "<img src='" + shanghai_1937.image + "' class='img' alt=''>";
+
+///////////////////////////////////
+//   ////////////////
+const kangzhan = {
+  title: "Kangzhan",
+  subtitle: "Guide to Chinese Ground Forces 1937–45",
+  author: "Leland Ness",
+  synopsis:
+    "Kangzhan: Guide to Chinese Ground Forces 1937–45 is the first ready reference to the organization and armament of Chinese ground forces during the Sino-Japanese War of 1937–45. The work integrates Chinese, Japanese and Western sources to examine the details of the structure and weapons of the period. Recent scholarship has contributed greatly to our understanding of China's role in the war, but this is the first book to deal with the bottom-level underpinnings of this massive army, crucial to an understanding of its tactical and operational utility.<br><br/>An introductory chapter discusses the military operations in China, often given short shrift in World War II histories. The work then traces the evolution of the national army's organizational structure from the end of the Northern Expedition to the conclusion of World War II. Included are tables of organization and strength reports for the wartime period.<br><br/>The armament section illustrates and details not only the characteristics of the many and varied weapons used in China, many seen nowhere else, but also their acquisition and such local production as was undertaken. This is complemented by a chapter on the arsenals and their evolution and production programs.<br><br/>The Chinese army was one of the largest of the war and it, and Japan's, fought longer than any other. It faced unique challenges, including fragmented loyalties, huge expanses of territory, poor logistics networks, inadequate arms supplies, and, often, incompetence and corruption. Nevertheless, they fought bravely in major battles through 1941 and were able to counterpunch effectively in important regions through the rest of the war. Aimed at both military historians and wargamers, this work fills an important gap in our understanding of this, the most under-appreciated army of the war.",
+  image: "./images/kangzhan.jpg",
+};
+
+document.getElementById("kangzhan_title").innerHTML = kangzhan.title;
+
+document.getElementById("kangzhan_subtitle").innerHTML = kangzhan.subtitle;
+
+document.getElementById("kangzhan_author").innerHTML = kangzhan.author;
+
+document.getElementById("kangzhan_synopsis").innerHTML = kangzhan.synopsis;
+
+document.getElementById("kangzhan_image").innerHTML =
+  "<img src='" + kangzhan.image + "' class='img' alt=''>";
+
+///////////////////////////////////
