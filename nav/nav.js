9505d9d1c0caa68ca3faae622095ce844d9ee670
Nathanial Walton <nathanial28@gmail.com>
2022-09-22 19:34:19 -0600

    update some style and fixed some styling bugs

--- nav/nav.js
+++ nav/nav.js
@@ -2,7 +2,7 @@
   const nav =
     "<div class='nav-center'><div class='navbar'><a href='/index.html'><h4 class='logo'>logo</h4></a><button type='button' class='bars' id='bars'><i class='fa fa-bars'></i></button></div><ul class='nav-links' id='nav-links'><li><a href='/index.html'>home</a></li><li><a href='/about.html'>about</a></li><li><a href='/contact.html'>contact</a></li></ul></div>";
 
-  document.getElementById("nav-bar").innerHTML = nav;
+  document.getElementById("nav-bar").insertAdjacentHTML("beforeend", nav);
 
   const getElement = (selector) => {
     const el = document.querySelector(selector);
@@ -17,4 +17,12 @@
     console.log("henlo");
     links.classList.toggle("show-links");
   });
+
+  /*
+  
+  <link rel="stylesheet" href="/nav/style.css" />
+  <nav id="nav-bar"></nav>
+  <script src="/nav/nav.js"></script>
+
+  */
 }
