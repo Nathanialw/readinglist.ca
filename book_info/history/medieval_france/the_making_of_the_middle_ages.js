391b10ed43c44806041ce97cd9c28254388d1128
Nathanial Walton <nathanial28@gmail.com>
2022-09-24 12:14:36 -0600

    added medieval france js data

---
+++ book_info/history/medieval_france/the_making_of_the_middle_ages.js
@@ -0,0 +1,18 @@
+const the_making_of_the_middle_ages = {
+  title: "the making of the middle ages",
+  subtitle: "",
+  author: "r. w. southern",
+  synopsis:
+    "Distinguished historian presents an absorbing study of the main personalities and the influences that molded the history of Western Europe from the late tenth to the early thirteenth centuries—the formative period of modern Western civilization. R. W. Southern describes the chief forms of social, political, and religious organization, analyzing a wealth of concrete, highly significant episodes. He recaptures the personal experiences and the individual consciences of the men who guided 'the making of the Middle Ages.'",
+  image:
+    "<img src='/assets/images/books/non-fiction/history/france/medieval_france/the-making-of-the-middle-ages.jpg' class='img' alt=''>",
+  link_pdf:
+    "<a class='fa fa-file-pdf' href='https://b-ok.cc/book/1067431/10c4d6' target='_blank'></a>",
+  link_epub:
+    "<a class='fa fa-book-open' href='https://b-ok.cc/book/1259427/7d0770' target='_blank' ></a>",
+  link_amazon:
+    "<a class='fab fa-amazon' href='https://www.amazon.ca/Making-Middle-Ages-R-Southern/dp/0300002300/ref=sr_1_1?keywords=The+Making+Of+The+Middle+Ages&qid=1663529145&sr=8-1' target='_blank' ></a>",
+  link_indigo:
+    "  <a class='' href='https://www.chapters.indigo.ca/en-ca/books/the-making-of-the-middle/9780300002300-item.html?ikwid=The+Making+of+the+Middle+Ages&ikwsec=Home&ikwidx=0#algoliaQueryId=99ba5bc93bcab416515ee0afae364748' target='_blank'>indigo</a>",
+  link_handmade: "<a class='fa fa-book' href='#' target='_blank'></a>",
+};
---
+++ book_info/history/sino-japanese_war/shanghai_1937.js
@@ -0,0 +1,12 @@
+const shanghai_1937 = {
+  title: "",
+  subtitle: "",
+  author: "",
+  synopsis: "",
+  image: "<img src='/assets/images/books' class='img' alt=''>",
+  link_pdf: "<a class='fa fa-file-pdf' href='' target='_blank'></a>",
+  link_epub: "<a class='fa fa-book-open' href='' target='_blank' ></a>",
+  link_amazon: "<a class='fab fa-amazon' href='' target='_blank' ></a>",
+  link_indigo: "  <a class='' href='' target='_blank'>indigo</a>",
+  link_handmade: "<a class='fa fa-book' href='#' target='_blank'></a>",
+};
